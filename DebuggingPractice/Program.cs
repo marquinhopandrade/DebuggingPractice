0950257ba67d6c3b3b7350df4abf53f4fb62cbc0
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-03-06 17:02:39 -0300

    Adicionar arquivos de projeto.

---
+++ DebuggingPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebuggingPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
+            var partyFriends = GetPartyFriends(friends, 3);
+            foreach (var name in partyFriends)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static List<string> GetPartyFriends(List<string> list, int count)
+        {
+            var partyFriends = new List<string>();
+            while (partyFriends.Count < count)
+            {
+                var currentFriend = GetPartyFriend(list);
+                partyFriends.Add(currentFriend);
+                list.Remove(currentFriend);
+            }
+            return partyFriends;
+        }
+
+        public static string GetPartyFriend(List<string> list)
+        {
+            string shortestName = list[0];
+            for(var i = 0; i < list.Count;i++)
+            {
+                if (list[i].Length < shortestName.Length)
+                {
+                    shortestName = list[i];
+                }
+            }
+                return shortestName;
+        }
+    }
+}
